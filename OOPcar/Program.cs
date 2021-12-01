99ff573a6308b183cd6af12760190f2e1e4d6817
Opilane TTHK <opilane@tthk.ee>
2021-12-01 10:24:43 +0200

    OOPpet and car

---
+++ OOPcar/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace OOPcar
+{
+    class Program
+    {
+        class Car
+        {
+            string mark;
+            string model;
+            string registration;
+            string color;
+            int odometer;
+            int fuel;
+            
+            public Car(string _mark,string _model,string _registration, string _color)
+            {
+                mark = _mark;
+                model = _model;
+                registration = _registration;
+                color = _color;
+                odometer = 0;
+                fuel = 60;
+                Console.WriteLine($"The {color} {mark} {model} has been created");
+            }
+
+        }
+        
+        static void Main(string[] args)
+        {
+            Car myCar = new Car("Mazda", "RX-7", "848Y", "Black");
+        }
+    }
+}
---
+++ OOPpet/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace OOPpet
+{
+    class Program
+    {
+        class Pet
+        {
+            string name;
+            int age;
+            double weight; 
+            
+            public Pet(string _name,int _age)
+            {
+                name = _name;
+                age = _age;
+                weight = 0;
+                Console.WriteLine($"The pet named {name} '{age} years old' who weighs {weight} has been created.");
+            }
+            public string Name
+            {
+                get { return name; }
+            }
+            public int Age
+            {
+                get { return age; }
+            }
+            public double Weight
+            {
+                get { return weight; }
+            }
+            public void Eating()
+            {
+                Console.WriteLine("Nom-Nom");
+                weight += 0.2;
+            }
+            public void Exercise()
+            {
+                Console.WriteLine("*running*");
+                weight -= 0.1;
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Pet myPet = new Pet("Hamster", 1);
+            Console.WriteLine($"Weight: {myPet.Weight}");
+
+            for (int i = 0; i > 11; i++)
+            {
+                myPet.Eating();
+                Console.WriteLine($"Current weight: {myPet.Weight}");
+                if(myPet.Weight > 0.7)
+                {
+                    myPet.Exercise();
+                }
+            }
+        }
+    }
+}
